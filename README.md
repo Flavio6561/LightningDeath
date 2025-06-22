44cb8736e58b42dd5c9f5d8d9b6cff2c02f601d5
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2025-06-22 18:59:16 +0200

    Version 2.1.1 Full Changelog: https://github.com/Flavio6561/LightningDeath/wiki/Version-changelogs

--- README.md
+++ README.md
@@ -6,20 +6,13 @@ a mod that spawns a lightning bolt on a player death position
 > ![ModShowcaseGif](https://raw.githubusercontent.com/Flavio6561/Gallery/refs/heads/main/LightningDeath/Lightning.gif)  
 > `A lightning bolt is summoned on the player death position`
 
-### Config:
-
-> - `/ld` toggles the mod on or off
-> - `/ld IncludePlayer` toggles bolts summoning on playing character
-
 ### Other:
-
-> Settings will be saved in your config folder and updated at runtime
 
 > The mod is client side (works on multiplayer)
 
 ### Links:
 
-> - [GitHub page](https://github.com/Flavio6561/LightningDeath)  
-> - [Modrinth page](https://modrinth.com/mod/lightningdeath)  
-> - [Changelogs](https://github.com/Flavio6561/LightningDeath/wiki/Version-changelogs)  
+> - [GitHub page](https://github.com/Flavio6561/LightningDeath)
+> - [Modrinth page](https://modrinth.com/mod/lightningdeath)
+> - [Changelogs](https://github.com/Flavio6561/LightningDeath/wiki/Version-changelogs)
 > - [Report an issue](https://github.com/Flavio6561/LightningDeath/issues)
--- src/client/java/com/lightningdeath/LightningDeathClient.java
+++ src/client/java/com/lightningdeath/LightningDeathClient.java
@@ -1,44 +1,37 @@
 package com.lightningdeath;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LightningDeathClient implements ClientModInitializer {
-	private static final Logger LOGGER = LoggerFactory.getLogger(LightningDeathClient.class);
 	private static final List<PlayerEntity> ignoredPlayers = new ArrayList<>();
 
 	@Override
 	public void onInitializeClient() {
-		WorldRenderEvents.END.register(context -> lightningDeathMain());
-		LOGGER.info("LightningDeath initialized");
+		WorldRenderEvents.END.register(LightningDeathClient::lightningDeathMain);
 	}
 
-	private static void lightningDeathMain() {
-		ClientWorld world = MinecraftClient.getInstance().world;
-		if (world == null)
-			return;
-        List<PlayerEntity> toAdd = new ArrayList<>();
-		List<PlayerEntity> toRemove = new ArrayList<>();
+	private static void lightningDeathMain(WorldRenderContext context) {
+		ArrayList<PlayerEntity> toAdd = new ArrayList<>();
+		ArrayList<PlayerEntity> toRemove = new ArrayList<>();
 
-		for (PlayerEntity player : world.getPlayers()) {
+		for (PlayerEntity player : context.world().getPlayers()) {
 			if (!player.isDead())
 				toRemove.add(player);
 			else if (!ignoredPlayers.contains(player)) {
 				toAdd.add(player);
-				Entity lightningBolt = EntityType.LIGHTNING_BOLT.create(world, null);
-				if (lightningBolt != null)
+				Entity lightningBolt = EntityType.LIGHTNING_BOLT.create(context.world(), null);
+				if (lightningBolt != null) {
 					lightningBolt.updatePosition(player.getX(), player.getY(), player.getZ());
-                world.addEntity(lightningBolt);
+					context.world().addEntity(lightningBolt);
+				}
 			}
 		}
         ignoredPlayers.addAll(toAdd);
