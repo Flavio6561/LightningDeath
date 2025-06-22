decbea84a67d8a5952d2173e42a969661da30f0d
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2025-06-22 18:42:49 +0200

    Version 2.1.0 Full Changelog: https://github.com/Flavio6561/LightningDeath/wiki/Version-changelogs

--- src/client/java/com/lightningdeath/LightningDeathClient.java
+++ src/client/java/com/lightningdeath/LightningDeathClient.java
@@ -1,75 +1,47 @@
 package com.lightningdeath;
 
-import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LightningDeathClient implements ClientModInitializer {
-	protected static boolean toggleMod = true;
-	protected static boolean includePlayer = true;
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(LightningDeathClient.class);
 	private static final List<PlayerEntity> ignoredPlayers = new ArrayList<>();
 
 	@Override
 	public void onInitializeClient() {
-		ConfigManager.loadConfig();
-		CommandRegister.registerCommands();
-		WorldRenderEvents.END.register(context -> manageLightningDeaths(MinecraftClient.getInstance()));
-		LOGGER.info("LightningDeath initialized with success");
+		WorldRenderEvents.END.register(context -> lightningDeathMain());
+		LOGGER.info("LightningDeath initialized");
 	}
 
-	private void manageLightningDeaths(MinecraftClient client) {
-		ClientWorld world = client.world;
-		if (world == null || !toggleMod)
+	private static void lightningDeathMain() {
+		ClientWorld world = MinecraftClient.getInstance().world;
+		if (world == null)
 			return;
-
+        List<PlayerEntity> toAdd = new ArrayList<>();
 		List<PlayerEntity> toRemove = new ArrayList<>();
-		List<PlayerEntity> toAdd = new ArrayList<>();
 
 		for (PlayerEntity player : world.getPlayers()) {
 			if (!player.isDead())
 				toRemove.add(player);
-			else if (!ignoredPlayers.contains(player) && (!player.isMainPlayer() || includePlayer)) {
+			else if (!ignoredPlayers.contains(player)) {
 				toAdd.add(player);
 				Entity lightningBolt = EntityType.LIGHTNING_BOLT.create(world, null);
-				if (lightningBolt != null) {
+				if (lightningBolt != null)
 					lightningBolt.updatePosition(player.getX(), player.getY(), player.getZ());
-					world.addEntity(lightningBolt);
-				}
+                world.addEntity(lightningBolt);
 			}
 		}
+        ignoredPlayers.addAll(toAdd);
 		ignoredPlayers.removeAll(toRemove);
-		ignoredPlayers.addAll(toAdd);
-	}
-
-	public static void sendMessageToPlayer(String chat_message) {
-		MinecraftClient client = MinecraftClient.getInstance();
-		if (client.player != null)
-			client.player.sendMessage(Text.literal(chat_message), false);
-	}
-
-	public static int toggleMod(CommandContext<FabricClientCommandSource> ignoredFabricClientCommandSourceCommandContext) {
-		toggleMod = !toggleMod;
-		ConfigManager.saveConfig();
-		sendMessageToPlayer("Mod is now " + (toggleMod ? "enabled" : "disabled"));
-		return 1;
-	}
-
-	public static int toggleIncludePlayer(CommandContext<FabricClientCommandSource> ignoredFabricClientCommandSourceCommandContext) {
-		includePlayer = !includePlayer;
-		ConfigManager.saveConfig();
-		sendMessageToPlayer("Lightning bolts will " + (includePlayer ? "" : "NOT ") + "spawn on playing character");
-		return 1;
 	}
 }
