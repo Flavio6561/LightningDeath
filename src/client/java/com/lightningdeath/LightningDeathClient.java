654fae07f58b7863e34d038fce056185429fbdfd
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2025-10-01 20:50:03 +0200

    Version 2.1.0_1.21.9 Full Changelog: https://github.com/Flavio6561/LightningDeath/wiki/Version-changelogs

--- src/client/java/com/lightningdeath/LightningDeathClient.java
+++ src/client/java/com/lightningdeath/LightningDeathClient.java
@@ -1,11 +1,12 @@
 package com.lightningdeath;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 
 import java.util.ArrayList;
 
@@ -14,26 +15,26 @@ public class LightningDeathClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		WorldRenderEvents.END.register(LightningDeathClient::lightningDeathMain);
+        ClientTickEvents.END_WORLD_TICK.register(LightningDeathClient::lightningDeathMain);
 	}
 
-	private static void lightningDeathMain(WorldRenderContext context) {
-		ArrayList<PlayerEntity> toAdd = new ArrayList<>();
-		ArrayList<PlayerEntity> toRemove = new ArrayList<>();
+    private static void lightningDeathMain(ClientWorld clientWorld) {
+        ArrayList<PlayerEntity> toAdd = new ArrayList<>();
+        ArrayList<PlayerEntity> toRemove = new ArrayList<>();
 
-		for (PlayerEntity player : context.world().getPlayers()) {
-			if (!player.isDead())
-				toRemove.add(player);
-			else if (!ignoredPlayers.contains(player)) {
-				toAdd.add(player);
-				Entity lightningBolt = EntityType.LIGHTNING_BOLT.create(context.world(), null);
-				if (lightningBolt != null) {
-					lightningBolt.updatePosition(player.getX(), player.getY(), player.getZ());
-					context.world().addEntity(lightningBolt);
-				}
-			}
-		}
+        for (PlayerEntity player : ((World) clientWorld).getPlayers()) {
+            if (!player.isDead())
+                toRemove.add(player);
+            else if (!ignoredPlayers.contains(player)) {
+                toAdd.add(player);
+                Entity lightningBolt = EntityType.LIGHTNING_BOLT.create(clientWorld, null);
+                if (lightningBolt != null) {
+                    lightningBolt.updatePosition(player.getX(), player.getY(), player.getZ());
+                    clientWorld.addEntity(lightningBolt);
+                }
+            }
+        }
         ignoredPlayers.addAll(toAdd);
-		ignoredPlayers.removeAll(toRemove);
-	}
+        ignoredPlayers.removeAll(toRemove);
+    }
 }
